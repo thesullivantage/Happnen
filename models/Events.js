cb557e7341231957e2541610d0f930a0c2f927b2
Your Name <jack.sullivan96@gmail.com>
2018-08-26 00:04:16 -0400

    2nd model

--- models/Events.js
+++ models/Events.js
@@ -16,8 +16,10 @@ const eventSchema = new Schema({
   //Also, combined date and time just based on the format; will concat when making the call
   datetime: { type: Date, default: Date.now },
   qr: { type: String, required: false },
+  //add id's here upon invitation by event creator:
   invited: [ { type: Schema.Types.ObjectId, ref: 'Users' } ],
   //probably disable inviting guests under 21 when we render the suggestions to invite guests, and if it is based on an "on submit" type of deal where they type in the user name, we could add in some error handling to let them know that the person is under 21
+  //add id's here upon user RSVP
   attending: [ { type: Schema.Types.ObjectId, ref: 'Users' } ],
   description: { type: String, required: false },
   img: [{type: String}],
