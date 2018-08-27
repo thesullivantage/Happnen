91bf392006323dd030ec528dad883248c317ae9f
inkspot <Wyattsimpson777@gmail.com>
2018-08-27 18:08:41 -0400

    AHHH

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Map.css';
-// import { GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react';
+
+import PropTypes from 'prop-types';
 
 
 export class Map extends React.Component {
     componentDidMount() {
-        this.loadMap();
+        // this.loadMap();
     }
 
     loadMap() {
@@ -28,26 +29,27 @@ export class Map extends React.Component {
    
         }
         
-        Map.propTypes = {
-            google: React.PropTypes.object,
-            zoom: React.PropTypes.number,
-            initialCenter: React.PropTypes.object
-        }
-    
-        Map.defaultProps = {
-            zoom: 13,
-            // San Francisco, by default
-            initialCenter: {
-              lat: 37.774929,
-              lng: -122.419416
-            }
-        }
     }
-
-
+    
+    
     render() {
         return (
             <div ref="map"></div>
         )
+    }
+}
+
+Map.propTypes = {
+    google: PropTypes.object,
+    zoom: PropTypes.number,
+    initialCenter: PropTypes.object
+}
+
+Map.defaultProps = {
+    zoom: 13,
+    // San Francisco, by default
+    initialCenter: {
+        lat: 37.774929,
+        lng: -122.419416
     }
 }
