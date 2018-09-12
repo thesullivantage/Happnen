a4f8190dd8bb38cb1558408f3b2c332b2b49c07f
ACE Merc <acendelman@gmail.com>
2018-09-11 22:30:26 -0400

    random edits and preloader

--- client/src/pages/MapDisplay/MapDisplay.js
+++ client/src/pages/MapDisplay/MapDisplay.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Input, Icon, Button } from "react-materialize";
+import { Container, Row, Col, Input, Icon, Button, Preloader } from "react-materialize";
 import "./MapDisplay.css";
 import { MapComponent } from "../../components/MapContainer/MapContainer";
 import API from "../../utils/API"
@@ -8,13 +8,30 @@ import UserFab from "../../components/UserFab";
 import NonUserFab from "../../components/NonUserFab";
 
 class MapDisplay extends Component {
+    state = {
+        loading: true
+    }
+
     componentDidMount() {
+        setTimeout(() => this.setState({ loading: false }), 1500);
         console.log("Hello out there?")
     }
     render() {
-            return (
-                <MapComponent />
-            );
+
+
+        const { loading } = this.state;
+
+        if (loading) {
+            return <Col className="loader" s={4}>
+                <Preloader
+                    flashing="true"
+                    size='big' />
+            </Col>;
+        }
+
+        return (
+            <MapComponent />
+        );
     }
 }
 
